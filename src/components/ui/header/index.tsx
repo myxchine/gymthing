f8cc5ae859addb982f3f3bb3f3a30b36b70571a2
Michael Dos Santos <michael.dozzantoz@gmail.com>
2025-03-10 10:58:14 +0000

    fixed mobile header rounding shift on menu open

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -4,7 +4,7 @@ import Desktop from "./desktop";
 export default function Header() {
   return (
     <header className="flex flex-col w-full sticky top-0 z-[1000000] bg-transparent  p-2 md:p-4">
-      <div className=" w-full px-4 pr-2 py-1 md:px-5 md:py-3    inset-0 z-10  max-w-4xl mx-auto rounded-3xl bg-black/5 backdrop-blur-lg">
+      <div className=" w-full px-4 pr-2 py-1 md:px-5 md:py-3    inset-0 z-10  max-w-4xl mx-auto rounded-[20px] md:rounded-full bg-black/5 backdrop-blur-lg">
         <Mobile />
         <Desktop />
       </div>
